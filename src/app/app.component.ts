ff9412e86c3bac8430d341f640aca837d4578464
Unknown <javi_vavi@hotmail.com>
2019-01-29 16:00:08 +0100

    Data Binding Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My First Angular App';
+
+  /* Primera Prueba Binding */
+  onClickMe(mess) {
+    alert(mess);
+  }
+
+  clickMess = "";
+  clicks = 0;
+
+  onClickMe2(event: any) {
+    let evMsg = event ? " Event target is " + event.target.tagName : "";
+    this.clickMess = (`Click #${this.clicks++}. ${evMsg}`);
+  }
 }
